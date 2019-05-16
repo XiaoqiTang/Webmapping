cbc66b9e948ea2f3f2045c1bb25f63fd9b644a9c
XiaoqiTang <xiaoqit@design.upenn.edu>
2019-05-16 10:49:07 -0400

    all materials

---
+++ js/drawchart.js
@@ -0,0 +1,52 @@
+
+var horizontaloption = {
+      responsive:true,
+      maintainAspectRatio: false,
+      scales: {
+          xAxes: [{
+              ticks: {
+                  display:false
+              },
+              scaleLabel:{
+                  display:false
+              },
+              gridLines: {
+                display:false
+              },
+              stacked: true
+          }],
+          yAxes: [{
+              gridLines: {
+                display:false,
+              },
+              ticks: {
+                  fontFamily: "'Open Sans Bold', sans-serif",
+                  fontSize:11
+              },
+              stacked: true
+          }]
+      },
+      legend:{
+          display:false
+      }
+  };
+
+var ctx1 = document.getElementById("Chart1");
+
+
+var ctx2 = document.getElementById("Chart2");
+
+
+var ctx3 = document.getElementById("Chart3");
+
+
+var ctx4 = document.getElementById("Chart4");
+
+
+var ctx5 = document.getElementById("Chart5");
+
+
+
+
+
+
